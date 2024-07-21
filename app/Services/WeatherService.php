f0c6aff97a4ab4d99db77661f5f3937e7d12c129
dosidat1503 <dosidat15031712@gmail.com>
2024-07-21 08:54:21 +0700

    update route and env to deploy

--- app/Services/WeatherService.php
+++ app/Services/WeatherService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use GuzzleHttp\client;
+use Illuminate\Support\Facades\Log;
 
 class WeatherService{
     protected $client;
@@ -11,23 +12,48 @@ class WeatherService{
     public function __construct(){
         $this->client = new Client();
         $this->apiKey = env('WEATHER_API_KEY');
+        Log::info('WEATHER_API_KEY: ' . $this->apiKey);
     }
 
     public function getWeather($location, $forecastDayQuantity){
+        // $response = $this->client->request(
+        //     'GET', 
+        //     'http://api.weatherapi.com/v1/forecast.json',
+        //     [
+        //         'query' => [
+        //             'key' => $this->apiKey,
+        //             'q' => $location,
+        //             'days' => $forecastDayQuantity,
+        //             'aqi' => 'no',
+        //             'alerts' => 'no',
+        //         ]
+        //     ]
+        // ); 
+
+        // return json_decode($response->getBody()->getContents(), true);
+
+
+        $queryParams = [
+            'key' => $this->apiKey,
+            'q' => $location,
+            'days' => $forecastDayQuantity,
+            'aqi' => 'no',
+            'alerts' => 'no',
+        ];
+    
+        $url = 'http://api.weatherapi.com/v1/forecast.json?' . http_build_query($queryParams);
+    
+        // Log the request URL
+        Log::info("Request URL: " . $url);
+    
         $response = $this->client->request(
             'GET', 
             'http://api.weatherapi.com/v1/forecast.json',
             [
-                'query' => [
-                    'key' => $this->apiKey,
-                    'q' => $location,
-                    'days' => $forecastDayQuantity,
-                    'aqi' => 'no',
-                    'alerts' => 'no',
-                ]
+                'query' => $queryParams
             ]
         ); 
-
+    
         return json_decode($response->getBody()->getContents(), true);
     }
 
